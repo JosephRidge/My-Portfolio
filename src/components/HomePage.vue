1a9a76d7d056f700c195e6f0a1fbde782eff75f1
josephridge <josephridge18@gmail.com>
2022-03-22 01:14:12 +0300

    Update on Home page

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,29 +1,32 @@
 <template>
   <div class="bg-primary w-screen h-screen static">
     <!-- background -->
-    <div>
-      <img
+    <div class="transition duration-300 ease-in-out">
+
+            <img
         src="src/assets/bgImage.png"
-        class="h-screen w-1/2"
+        class=" h-screen w-1/2 hover:bg-red-900"
         alt="image of Pencil and rubber on a green background"
         srcset=""
-      />
+      /> 
+      
       <!-- Center image + Name-->
       <div
-        class="absolute flex flex-col justify-center top-0 right-0 bottom-0 left-0"
+        class="absolute w-fit  flex flex-col justify-center top-0 right-0 bottom-0 left-0"
       >
-        <div class="flex justify-center">
+        <div class="flex justify-center
+       p-2 hover:hidden   ">
           <img
             src="src/assets/Logo.svg"
-            class="w-1/5"
+            class=""
             alt="Joseph Ridge logo"
             srcset=""
           />
         </div>
-        <div class="flex justify-center">
-              <img
+        <div class="flex justify-center ">
+          <img
             src="src/assets/JayR.svg"
-            class="w-1/5 mb-20"
+            class="w-1/5 pb-10 "
             alt="Joseph Ridge logo"
             srcset=""
           />
