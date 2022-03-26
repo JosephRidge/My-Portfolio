583f7e3821fcfb39a8ee6fe97876cd46e2da7c69
josephridge <josephridge18@gmail.com>
2022-03-26 19:10:51 +0300

    updates onn  the about content

--- src/mixins/defaultMixins.js
+++ src/mixins/defaultMixins.js
@@ -1,1 +1,31 @@
-// Commonly used Variables. contant values - images or Methods 
+// Commonly used Variables. contant values - images or Methods
+export default {
+  data() {
+    return {
+     whatamI:"I Mobile Applications Developer & Junior UI/UX designer living and working in Nairobi, Kenya"        ,
+      whoamI:
+        "I am a Software Engineer with 400HRS plus in active in UI/ UX design and solutions development. I am a quick study and this \
+      has helped me learn complex concepts in a very short time. I put in the effort on a daily \
+      to cultivate and grow my meticulous nature as it lays an important role in innovation. ",
+      mypassion:
+        "I Have a passion for mobile software development, using both native and cross platform tools,\
+      and ovetime i have also been able to grow skills in building front-end web applications.\
+       I am driven by projects that are tied to a cause, especially capacity building in software\
+        development and or generally anything that will help push our societies forward. ",
+      myadavantage:
+        "I have been privilaged to interact with a number of technologies eg. \
+        Spectrum Technology (TVWS) & SDRs, Flutter , Android ( Java | Kotlin ) , Python, HTML , CSS, C++ and JS.",
+      myideal:
+        " My Ideal in life is to continuously improve my skills as a Software Engineer while \
+       I teach those that need to know how to create or build but do not know where and how to start.",
+      collaborate:
+        "I am open to collaborations both on a voluntary (only if tied to a cause) and non-voluntary \
+      basis.   I have been privilaged to be part of great teams that have greatly contributed to\
+       my growth as a software engineer and they have taught me to always strive to get ahead and\
+        build / improve myself",
+    };
+  },
+  created() {
+    console.log("Default Mixin set... ");
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     extend: {
       colors:{
         primary: '#D9F1E6', 
+        secondary:'#FCBCB8'
       }
     },
   },
