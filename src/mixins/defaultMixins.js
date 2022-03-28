1186ec746e8cf23f0eb1c41ab942c087e272d95d
josephridge <josephridge18@gmail.com>
2022-03-29 01:10:58 +0300

    Update on the USer interface , styling challenge on the cards width and height

--- src/mixins/defaultMixins.js
+++ src/mixins/defaultMixins.js
@@ -2,30 +2,37 @@
 export default {
   data() {
     return {
-     whatamI:"I Mobile Applications Developer & Junior UI/UX designer living and working in Nairobi, Kenya"        ,
+      whatamI:
+        "I am Junior Mobile Applications Developer & UI/UX designer living and working in Nairobi, Kenya",
       whoamI:
-        "I am a Software Engineer with 400HRS plus in active in UI/ UX design and solutions development. I am a quick study and this \
-      has helped me learn complex concepts in a very short time. I put in the effort on a daily \
-      to cultivate and grow my meticulous nature as it lays an important role in innovation. ",
+        "What would the world be like without abstractions ? Well in the solutions industry,\
+         not so pretty. \
+       I live and thrive in creating well thought out abstractions in the software realm \
+       that later on engender interest and curiosity within individuals.\
+       All through UI/UX design and Mobile Applications",
       mypassion:
-        "I Have a passion for mobile software development, using both native and cross platform tools,\
-      and ovetime i have also been able to grow skills in building front-end web applications.\
-       I am driven by projects that are tied to a cause, especially capacity building in software\
-        development and or generally anything that will help push our societies forward. ",
-      myadavantage:
-        "I have been privilaged to interact with a number of technologies eg. \
-        Spectrum Technology (TVWS) & SDRs, Flutter , Android ( Java | Kotlin ) , Python, HTML , CSS, C++ and JS.",
+        "I am deeply passionate about creating solutions that directly interact\
+         with human beings on a personal level\
+         and with the exponential rise of Smart Phones and devices ownership\
+         I beleive that more impact can be made through mobile devices and smart devices.  ",
+      myuiux:
+        " .",
       myideal:
         " My Ideal in life is to continuously improve my skills as a Software Engineer while \
        I teach those that need to know how to create or build but do not know where and how to start.",
       collaborate:
-        "I am open to collaborations both on a voluntary (only if tied to a cause) and non-voluntary \
-      basis.   I have been privilaged to be part of great teams that have greatly contributed to\
-       my growth as a software engineer and they have taught me to always strive to get ahead and\
-        build / improve myself",
+        "I regard it favourably as it combines empathy, diversity and teamwork. Hence, \
+         I am open to collaborations both on a voluntary (only if tied to a cause) and non-voluntary \
+         basis. ",
+      myskills:
+        " Equiped with problem solving skills and my Meticulous nature, I have been able to grow skills using the following technologie : \
+          Android-Studio (Java | Kotlin ), Flutter, HTML, CSS, JavaScript, Python, VueJS, Figma, Balsamiq, AdobeXD  \
+         ",
     };
   },
   created() {
     console.log("Default Mixin set... ");
   },
 };
+//  I achieve all these through  UI/UX design and later on building it. \
+//    to cultivate and grow my meticulous nature as it lays an important role in innovation. ",
