e3dd6785e06345348b37af293ee846a541e61853
josephridge <josephridge18@gmail.com>
2022-04-23 23:38:12 +0300

    WorkPage set up

---
+++ src/components/WorkPage.vue
@@ -0,0 +1,21 @@
+<template> 
+<div class="h-screen">
+<!-- Top Nav -->
+  <div> dgkjabfkabskb </div>
+  <!-- my title -->
+  <div></div>
+  <!-- work section -->
+  <div></div>
+
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
