da87d957422e0c15686a47fb8b33a2cc756bdda8
josephridge <josephridge18@gmail.com>
2022-05-08 22:36:34 +0300

    Blogs Page FInal working

--- src/main.js
+++ src/main.js
@@ -11,7 +11,32 @@ import Contact from "./components/ContactForm.vue";
 import App from "./App.vue";
 
 import "./style/index.css";
+ 
+/* Firebase config setup Start */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBUyJT6qrAs9vWuuNipsS-BFR2Tpt7JD3Y",
+  authDomain: "jayrportfolio.firebaseapp.com",
+  databaseURL: "https://jayrportfolio-default-rtdb.firebaseio.com",
+  projectId: "jayrportfolio",
+  storageBucket: "jayrportfolio.appspot.com",
+  messagingSenderId: "854566115082",
+  appId: "1:854566115082:web:0fbf483788915430394cf3",
+  measurementId: "G-XV0TLY9MWN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+/* Firebase config setup End */
 //Route records : routes for navigations using vue-router, each route maps to a component
 const routes = [
   { path: "/", component: HomePage },
---
+++ src/mixins/firebaseMixins.js
@@ -0,0 +1,27 @@
+import { initializeApp } from 'firebase/app';
+ 
+// Commonly used Variables. contant values - images or Methods
+export default {
+  data() {
+    return {
+      // firebaseConfig = {
+        apiKey: "AIzaSyBUyJT6qrAs9vWuuNipsS-BFR2Tpt7JD3Y",
+        authDomain: "jayrportfolio.firebaseapp.com",
+        databaseURL: "https://jayrportfolio-default-rtdb.firebaseio.com",
+        projectId: "jayrportfolio",
+        storageBucket: "jayrportfolio.appspot.com",
+        messagingSenderId: "854566115082",
+        appId: "1:854566115082:web:0fbf483788915430394cf3",
+        measurementId: "G-XV0TLY9MWN"
+      // }
+    };
+  },
+  created() {
+    console.log("Firebase Mixin set... ");
+  },
+};
+
+
+// Photo by Suzy Hazelwood: https://www.pexels.com/photo/man-writing-on-notebook-while-sitting-on-wooden-handrail-3091193/ -- thinking man
+// Photo by cottonbro: https://www.pexels.com/photo/person-in-white-shirt-with-brown-wooden-frame-4769486/ -- < book on face
+// Photo by Yurii Hlei: https://www.pexels.com/photo/eyeglasses-with-black-frames-on-top-of-book-1595822/ -- reading glasses
