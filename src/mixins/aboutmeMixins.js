ed542e3047a8ad232bac18d44d8c2ecabff8d070
josephridge <josephridge18@gmail.com>
2022-05-21 09:57:48 +0300

    Adsense

--- src/mixins/aboutmeMixins.js
+++ src/mixins/aboutmeMixins.js
@@ -3,24 +3,27 @@ export default {
     data() {
       return {
         strength:
-          "It is a long established fact that a reader will be\
-           distracted by the readable content of a page when\
-            looking at its layout. The point of using Lorem\
-             Ipsum is that it has a more-or-less normal distribution\
-              of letters,\
-           as opposed to using 'Content here, content here',  ",
+          "I believe that my greatest strength, lies not within my technical skills but in my ability to endure \
+           challenging situations.This has trnsalted to furthur grow my ability to learn while still \
+           gradually developing my meticoulous nature. This has helped me learn technologies over shorter periods of time,\
+           learn and try understand why we are the way they we are and our true drivers in every ecospace we humans live and go.  \
+           My key driver is that one day, i will manage to teach many under served communities on how to build and create mindblowing\
+            technoogical innovatinos .\ ",
         tools:
-          "It is a long established fact that a reader will be distracted\
-           by the readable content of a page when looking at its layout. \
-           The point of using Lorem Ipsum is that it has a more-or-less\
-            normal distribution of letters, as opposed to using 'Content here, \
-            content here', ",
+          "To achieve my goals in terms of development I combine a myria of tools\
+           from afew ranging from timers, the pomodoro techique to technical tools. \
+           Some of the technical tools I use are Android Studio\
+          , VsCode / VSCodium , VueJs and Tailwind CSS. In terms of languages\
+          I am equipped with Kotlin | Dart | HTML | CSS | JavaScript.",
         mypassion:
-          "It is a long established fact that a reader will be distracted \
-          by the readable content of a page when looking at its layout.\
-           The point of using Lorem Ipsum is that it has a more-or-less \
-           normal distribution of letters, as opposed to using 'Content here,\
-            content here', ",
+          "Imagine a world where everyone could code, from our village to our children\
+           to our grandparents, to you...\
+          Imagine the quality of solutioms, the advancements humanity will have made..\
+          Every human being solves problems in a unique way, combining that with software development / or technoology\
+          opens doors to true evolution. I am a beleiver that if I get to teach people how to create through code\
+          I will have improved and simplified the life of an individual. Hence, my qoal is to give Quality Education\
+          through well orchestrated content, articles, resources basically everytime i get a chence to teach Code, Design Thinking\
+          I give it everything.  \ ",
         
       };
     },
